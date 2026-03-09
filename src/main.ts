8866b9f148ad581ef1cead6c3314462588b26f13
Anandakrishnan H <anandak19h@gmail.com>
2026-03-09 13:15:24 +0530

    refactor: add client url and add redis service

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: ['http://localhost:4200', 'https://www.article.urbantasker.online'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization,Cookie',
